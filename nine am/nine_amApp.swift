14239657952cfac3d74f7199fbb9e19a8bbf4115
desmond <desmond@designand.build>
2024-12-16 20:48:11 +0800

    Initial Commit

---
+++ nine am/nine_amApp.swift
@@ -0,0 +1,17 @@
+//
+//  nine_amApp.swift
+//  nine am
+//
+//  Created by Desmond Ho on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct nine_amApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

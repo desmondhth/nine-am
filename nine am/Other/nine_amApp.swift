75c650e684a24a0fd562aa2a6580e82801081e7d
desmondhth <desmond.hth@gmail.com>
2024-12-20 00:19:02 +0800

    main view update

--- nine am/Other/nine_amApp.swift
+++ nine am/Other/nine_amApp.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 @main
 struct nine_amApp: App {
+        init() {
+        // Add this to register the font
+        if let fontURL = Bundle.main.url(forResource: "Caveat-VariableFont_wght", withExtension: "ttf") {
+            CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, nil)
+        }
+    }
     var body: some Scene {
         WindowGroup {
             MainView()
--- nine am/View/MainView.swift
+++ nine am/View/MainView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 250/255, green: 251/255, blue: 244/255)
+              .ignoresSafeArea()
+            VStack {
+                Image("nineamlogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 70, height: 73)
+
+                Text(Date().formatted(.dateTime.weekday().day().month()))
+                    .font(.custom("Caveat-Bold", size: 36))
+                    .padding(.top, 8)
+                    .padding(.bottom, 8)  // Add bottom padding
+                    .frame(height: 40)   // Set a specific height to accommodate the full text
+
+            }
+            .frame(maxWidth: .infinity)
         }
-        .padding()
     }
 }
 
